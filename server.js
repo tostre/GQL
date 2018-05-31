dc34d5ee6d83540643f6a45206f1d1f0ce184a3c
tostre <marcel.mueller3101@gmail.com>
2018-05-31 18:17:29 +0200

    gg

--- server.js
+++ server.js
@@ -1,13 +1,25 @@
 // In der Datei wird ein einfacher Express-Server aufgesetzt
 const express = require("express");
-const graphqlHTTP = require("express-graphql");
+const nodejs = require("express-graphql");
 const schema = require("./schema.js");
 const server = new express();
 
-server.use("/schema", graphqlHTTP({
+server.use("/schema", nodejs({
     schema,
     graphiql: true
 }));
 
 server.listen(4000);
 console.log("Server listening for querys ...");
+
+
+
+
+
+
+
+
+
+
+
+
